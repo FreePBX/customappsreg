0a3b2644d1c0d2208cf0e889ff6f266b6d81dcf0
James Finstrom <jfinstrom@sangoma.com>
2018-07-16 17:45:11 +0000

    Backup and Restore fixes

--- Restore.php
+++ Restore.php
@@ -3,8 +3,8 @@ namespace FreePBX\modules\Customappsreg;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    $this->FreePBX->Customappsreg->setMiltiConfig($configs['destinations'], 'dests');
+    $configs = reset($this->getConfigs());
+    $this->FreePBX->Customappsreg->setMultiConfig($configs['destinations'], 'dests');
     foreach($configs['extensions'] as $extension){
         $this->FreePBX->Customappsreg->editCustomExten($extension['custom_exten'], $extension['custom_exten'], $extension['description'], $extension['notes']);
     }
