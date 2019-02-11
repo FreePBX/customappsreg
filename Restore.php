3ef90be2f1b7a3066b5789a252ad142bb2776745
Andrew Nagy <andrew.nagy@the159.com>
2019-02-11 13:32:24 -0500

    backup and restpre

--- Restore.php
+++ Restore.php
@@ -3,10 +3,10 @@ namespace FreePBX\modules\Customappsreg;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
 	public function runRestore($jobid){
-		$configs = reset($this->getConfigs());
+		$configs = $this->getConfigs();
 		$this->FreePBX->Customappsreg->setMultiConfig($configs['destinations'], 'dests');
 		foreach($configs['extensions'] as $extension){
-				$this->FreePBX->Customappsreg->editCustomExten($extension['custom_exten'], $extension['custom_exten'], $extension['description'], $extension['notes']);
+			$this->FreePBX->Customappsreg->editCustomExten($extension['custom_exten'], $extension['custom_exten'], $extension['description'], $extension['notes']);
 		}
 	}
 
