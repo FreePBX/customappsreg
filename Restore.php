4fd0d3966b23103f9e05d37c9ed0757995a9fa27
nthumann <me@n-thumann.de>
2019-08-17 21:25:12 +0000

    Related to FREEPBX-20414: removed $jobid from runRestore()

--- Restore.php
+++ Restore.php
@@ -2,7 +2,7 @@
 namespace FreePBX\modules\Customappsreg;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
+	public function runRestore(){
 		$configs = $this->getConfigs();
 		$this->importKVStore($configs['kvstore']);
 		foreach($configs['extensions'] as $extension){
